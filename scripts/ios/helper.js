60c26939fb3a44ffb05bd4fab18b5768ad1f4bc9
Augustine Cheong <ts-augustine.cheong@rakuten.com>
2019-12-03 18:46:02 +0900

    Added logs

--- scripts/ios/helper.js
+++ scripts/ios/helper.js
@@ -39,9 +39,10 @@ module.exports = {
     xcodeProject.parseSync();
 
     // Build the body of the script to be executed during the build phase.
-    var script = '"' + '\\"${PODS_ROOT}/Fabric/run "' + pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
-    // var script = '"' + "\\\"/Plugins/cordova-fabric-plugin/Fabric.framework/run " + pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
+    var script = '"' + '\\"${PODS_ROOT}/Fabric/run\\"' + pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
     console.log(script);
+    var otherscript = '"' + '\\"${SRCROOT}\\"' + "/\\\"" + utilities.getAppName(context) + "\\\"/Plugins/cordova-fabric-plugin/Fabric.framework/run " + pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
+    console.log(otherscript);
 
     // Generate a unique ID for our new build phase.
     var id = xcodeProject.generateUuid();
